37e9cad962b5699196759b6e1703f43fc4a83e57
Ramiro Olivera <ramiro.olivera@wolox.com.ar>
2015-08-10 18:07:31 -0300

    Code clean up

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,29 @@
-"use strict";
+/*global $ */
+'use strict';
+
+//Manejo de animaciones cuando abre el sitio
+function openBars(time){
+	$('.copyright').animate({
+		bottom: '+=200'
+	},time,function(){});
+
+	$('header').animate({
+		marginTop: '+=400'
+	},time,function(){});
+}
+
 var slide = 1;
 
 //Manejo del slideshow
-$(".backcontrols").hover(function(){
-	if ($("#Copyright").css("bottom") =="-200px"){
+$('.backcontrols').hover(function(){
+	if ($('.copyright').css('bottom') ==='-200px'){
 		openBars(1000);
 	}
 },function(){});
 
 
 function pasar(slide){
-	$("body").css("background-image", "url('../assets/slide" + slide + ".jpg')");
+	$('body').css('background-image', 'url("../assets/slide' + slide + '.jpg")');
 }
 
 
@@ -24,105 +37,106 @@ function slider(){
 }
 
 function fullscreen(){
-	$("#Copyright").animate({
-		bottom: "-=200"
+	$('.copyright').animate({
+		bottom: '-=200'
 	},1000,function(){});
 
-	$("header").animate({
-		marginTop: "-=400"
+	$('header').animate({
+		marginTop: '-=400'
 	},1000,function(){});
 }
 
 function siguiente(){
 	window.clearInterval(paso);
-	$("button.play").html("▻");
+	$('button.play').html('▻');
 	if(slide<4){
 		slide++;
 		pasar(slide);
-	}else if(slide ==4){
+	}else if(slide === 4){
 		pasar(slide=1); 
 	}
 }
 
 function anterior(){
 	window.clearInterval(paso);
-	$("button.play").html("▻");
+	$('button.play').html('▻');
 	if(slide>1){
 		slide--;
 		pasar(slide);
 	} else if(slide <= 1){
-		slide=4
+		slide=4;
 		pasar(slide);
 	}
 }
 
 
-$(".play").click(function(){
-	if($(this).html() == "▻"){
+$('.play').click(function(){
+	if($(this).html() === '▻'){
 		slider();
-		$("button.play").html("||");
+		$('button.play').html('||');
 	}else{
-		$("button.play").html("▻");
+		$('button.play').html('▻');
 		window.clearInterval(paso);
 	}
 });
 
+$('.prev').click(function(){
+	anterior();
+});
+
+$('.next').click(function(){
+	siguiente();
+});
+
+$('.fullsc').click(function(){
+	fullscreen();
+});
 
 //Manejo del toggle button
-$("nav button").click(function(){
-	if ($("#menu").css("display") == "none"){
-		$("#menu").css("display","block");
-		$( "#menu" ).animate({
-			height: "+=200"
+$('nav button').click(function(){
+	if (!$('.menu').hasClass('display')){
+		$('.menu').addClass('display');
+		$('.menu').animate({
+			height: '+=200'
 		}, 1000, function() {
 		// Animation complete.
 		});	
 
 		setTimeout(function(){
-			$( "#menu ul" ).animate({
-				marginTop: "+=150"
+			$( '.menu ul' ).animate({
+				marginTop: '+=150'
 			}, 1000, function() {
 			// Animation complete.
 			});	
 		},500);
 
 	}else{
-		$("#menu").css("display","none");
-		$("#menu").css("height","0");
-		$("#menu ul").css("margin-top","-150px");
+		$('.menu').removeClass('display');
+		$('.menu').css('height','0');
+		$('.menu ul').css('margin-top','-150px');
 	}
 });
 
 
 //Manejo del menu hover
-$("nav > ul li").hover(function(){
-	$("#submenu").css("display","none");
+$('nav > ul li').hover(function(){
+	$('.submenu').css('display','none');
 },function(){});
 
-$("nav > ul:nth-child(2) li:nth-child(1)").hover(function(){
-	$("#submenu").css("display","block");
+$('nav > ul:nth-child(2) li:nth-child(1)').hover(function(){
+	$('.submenu').css('display','block');
 },function(){});
 
-$("#submenu").hover(function(){},function(){
-	$("#submenu").css("display","none");
+$('.submenu').hover(function(){},function(){
+	$('.submenu').css('display','none');
 });
 
 
 //Manejo de animaciones cuando abre el sitio
-function openBars(time){
-	$("#Copyright").animate({
-		bottom: "+=200"
-	},time,function(){});
-
-	$("header").animate({
-		marginTop: "+=400"
-	},time,function(){});
-}
-
 function startUp(){
 	slider();
-	$("#Copyright").css("bottom","-200px");
-	$("header").css("margin-top","-400px");
+	$('.copyright').css('bottom','-200px');
+	$('header').css('margin-top','-400px');
 
 	openBars(2000);
 
@@ -132,15 +146,15 @@ $(document).ready(startUp());
 
 //Manejo de fotos con width chico.
 function cargarFotos(){
-	var toAppend = "";
+	var toAppend = '';
 	for(var i=1;i<5;i++){
-		toAppend += "<img src='assets/slide"+i+".jpg'></img>";
+		toAppend += '<img src="assets/slide'+i+'.jpg"></img>';
 	}
-	$("#Fotos").append(toAppend);
+	$('.fotos').append(toAppend);
 }
 
 function sacarFotos(){
-	$("#Fotos img").remove();
+	$('.fotos img').remove();
 }
 
 $(window).resize(function(){
@@ -150,4 +164,4 @@ $(window).resize(function(){
 	else {
 	   sacarFotos();
 	}
-})
+});
